2f9318f1032b41b5fa434e6906c20587adee9c41
Jamen <yhatotz19.ja@gmail.com>
2019-03-03 20:32:19 +0800

    Update ASQ

---
+++ asq/products/getAllProducts.php
@@ -0,0 +1,32 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: application/json');
+header('Access-Control-Allow-Methods: GET');
+header('Access-Control-Allow-Headers: Access-Control-Allow-Headers, Content-Type, Access-Control-Allow-Methods, Authorization, X-Requested-With');
+
+include_once '../conn.php';
+
+$products =$conn->query("SELECT * FROM products");
+
+$list = array();
+
+while ($row = $products->fetch_assoc()) {
+	extract($row);
+
+	$getInfo = $conn->query("SELECT * FROM product_type WHERE id='$type'");
+	$info = $getInfo->fetch_array();
+
+	$item = array(
+			'id' => $id,
+			'name' => $name,
+			'typeId' => $type,
+			'type' => $info['type'],
+			'description' => $description,
+			'price' => $price,
+			'stock' => $stock
+		);
+
+	array_push($list, $item);
+}
+
+echo json_encode($list);
